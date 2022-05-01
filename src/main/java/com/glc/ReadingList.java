0eabfd2286f1b6c44e103a03c8b2e8288fb55a27
Vardanyan <velison.vardanyan@teamsolutions.am>
2022-05-02 01:31:08 +0400

    AC_1_Green

--- src/main/java/com/glc/ReadingList.java
+++ src/main/java/com/glc/ReadingList.java
@@ -1,11 +1,12 @@
 package com.glc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadingList {
 
     public List<Book> getBooks()
     {
-        return null;
+        return new ArrayList<>();
     }
 }
