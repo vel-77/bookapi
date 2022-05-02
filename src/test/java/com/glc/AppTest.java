4dda33855e53fb2df49fe9a2204acd04758d4138
Vardanyan <velison.vardanyan@teamsolutions.am>
2022-05-02 12:57:44 +0400

    AC_3_Green

--- src/test/java/com/glc/AppTest.java
+++ src/test/java/com/glc/AppTest.java
@@ -18,22 +18,44 @@ public class AppTest {
         // SETUP
         ReadingList cut = new ReadingList();
         //Execute
-        List<Book> books = cut.getBooks();
+        List<BookItem> books = cut.getBooks();
         //Assert
-        assertEquals(0,books.size());
+        assertEquals(0, books.size());
     }
 
     @Test
-    public void onFirstBookAddedNumberReadShouldReturnOne()
-    {
+    public void onFirstBookAddedNumberReadShouldReturnOne() {
         //SETUP
         ReadingList cut = new ReadingList();
-        Book book = new Book("Harry Potter","Joan Rowling",478,10);//title,author,length,year
+        Book book = new Book("Harry Potter", "Joan Rowling", 478, 10);//title,author,length,year
 
         //Execute
-        cut.addBook(book,"September 3", 5);
+        cut.addBook(book, "September 3", 5);
 
         //ASSERT
         assertEquals(1, cut.numberRead());
     }
+
+    @Test
+    public void afterAddingAdditionalBooksNumberReadShouldReturnCorrectNumberOfBooks() {
+        //SETUP
+        ReadingList cut = new ReadingList();
+
+        Book book1 = new Book("Art of Loving", "Erich Fromm", 133, 4);
+        Book book2 = new Book("Art of Loving", "Erich Fromm", 133, 4);
+        Book book3 = new Book("Art of Loving", "Erich Fromm", 133, 4);
+        Book book4 = new Book("Art of Loving", "Erich Fromm", 133, 4);
+
+        cut.addBook(book1, "January 1", 4);
+        cut.addBook(book2, "January 1", 4);
+        cut.addBook(book3, "January 1", 4);
+        cut.addBook(book4, "January 1", 4);
+
+        //Execute
+        int numberOfBooks = cut.numberRead();
+
+        //ASSERT
+        assertEquals(4, numberOfBooks);
+    }
+
 }
